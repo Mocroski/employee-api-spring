cae14033ca6a81c9a590813c24ec531a00f1570f
LukeMocroski <devmocroski@gmail.com>
2023-02-02 09:27:59 -0300

    Created Spring Boot APP

---
+++ src/main/java/com/mocroski/Employee/EmployerApiApplication.java
@@ -0,0 +1,13 @@
+package com.mocroski.Employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mocroski/Employee/EmployerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mocroski.Employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
