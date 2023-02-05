0c5977d03a0057366483caafcf5affccb06480c0
LukeMocroski <devmocroski@gmail.com>
2023-02-05 10:10:01 -0300

    Config CORS

--- src/main/java/com/mocroski/Employee/controller/EmployeeController.java
+++ src/main/java/com/mocroski/Employee/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.mocroski.Employee.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mocroski.Employee.model.Employee;
 import com.mocroski.Employee.services.EmployeeService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
