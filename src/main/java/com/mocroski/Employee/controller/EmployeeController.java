9bab1ad48271dd661ba7798381a15b3cd4bb129d
LukeMocroski <devmocroski@gmail.com>
2023-02-07 10:53:05 -0300

    API completed

--- src/main/java/com/mocroski/Employee/controller/EmployeeController.java
+++ src/main/java/com/mocroski/Employee/controller/EmployeeController.java
@@ -1,11 +1,16 @@
 package com.mocroski.Employee.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,20 +23,43 @@ import com.mocroski.Employee.services.EmployeeService;
 @RequestMapping("/api/v1/")
 public class EmployeeController {
 
-	@Autowired
-	private EmployeeService employeeService;
+    private final EmployeeService employeeService;
 
-	public EmployeeController(EmployeeService employeeService) {
-		this.employeeService = employeeService;
-	}
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 
-	@PostMapping("/employees")
-	public Employee createEmployee(@RequestBody Employee employee) {
-		return employeeService.createEmployee(employee);
-	}
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.createEmployee(employee);
+    }
 
-	@GetMapping("/employees")
-	public List<Employee> getAllEmployees() {
-		return employeeService.getAllEmployees();
-	}
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return  employeeService.getAllEmployees();
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String,Boolean>> deleteEmployee(@PathVariable Long id) {
+        boolean deleted = false;
+        deleted = employeeService.deleteEmployee(id);
+        Map<String,Boolean> response = new HashMap<>();
+        response.put("deleted", deleted);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = null;
+        employee = employeeService.getEmployeeById(id);
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,
+                                                   @RequestBody Employee employee) {
+        employee = employeeService.updateEmployee(id, employee);
+        return ResponseEntity.ok(employee);
+    }
+
 }
--- src/main/java/com/mocroski/Employee/services/EmployeeService.java
+++ src/main/java/com/mocroski/Employee/services/EmployeeService.java
@@ -5,9 +5,15 @@ import java.util.List;
 import com.mocroski.Employee.model.Employee;
 
 public interface EmployeeService {
+    Employee createEmployee(Employee employee);
 
-	Employee createEmployee(Employee employee);
+    List<Employee> getAllEmployees();
 
-	List<Employee> getAllEmployees();
+    boolean deleteEmployee(Long id);
 
+    Employee getEmployeeById(Long id);
+
+	Employee updateEmployee(Long id, Employee employee);
 }
+
+    
